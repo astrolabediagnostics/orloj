9d26682eb7abf6f4a0375806ed8f6fe68977ac31
El-ad David Amir <el-ad@astrolabediagnostics.com>
2018-08-04 16:34:40 -0400

    Additional special case for desc

--- R/fcs.R
+++ R/fcs.R
@@ -91,6 +91,7 @@ isSample <- function(sample) {
   # data sets.
   desc[grep("CD56.*CD56", desc)] <- "CD56"
   desc[grep("CD45RA.*CD45RA", desc)] <- "CD45RA"
+  desc[grep("CD45RA.*Fluidigm", desc)] <- "CD45RA"
   desc
 }
 
